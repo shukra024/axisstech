fb35c5225cb558d7640d820bcae2e10834ccc9bf
shukra <shukrafathima024@gmail.com>
2020-10-10 17:41:23 +0530

    TDA-06

---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+$server = "localhost";
+$uname = "root";
+$pass = "";
+$dbname = "todo";
+
+$conn = mysqli_connect($server,$uname,$pass,$dbname);
+
+if(isset($_POST['submit']))
+{
+
+	$name = mysqli_real_escape_string($conn,$_POST['name']);
+	$password = mysqli_real_escape_string($conn,$_POST['password']);
+
+	if(empty($name)||empty($password))
+	{
+		echo "error empty field";
+	}
+
+	else
+	{
+		$sql = "SELECT * FROM user WHERE name = '$name'";
+		$result = mysqli_query($conn,$sql);
+		$resultCheck = mysqli_num_rows($result);
+
+		if($resultCheck < 1)
+		{
+			echo("no username found") ;
+		header("Location:./login.html?username=error");
+
+		}
+
+		else
+		{
+			$sql = "SELECT * from user WHERE password = '$password';";
+			$result = mysqli_query($conn,$sql);
+			$row = mysqli_fetch_assoc($result);
+
+			if($row['password'] == $password)
+			{
+				session_start();
+				$_SESSION['name']    = $row['name'];
+
+				header("Location:./home.html?Login=success");
+
+			}
+			else
+			{
+				echo "error";
+			}
+
+
+
+		}
+
+	}
+
+}
---
+++ signup.php
@@ -0,0 +1,67 @@
+<?php
+
+session_start();
+$server = "localhost";
+$uname = "root";
+$pass = "";
+$dbname = "todo";
+
+$conn = mysqli_connect($server,$uname,$pass,$dbname);
+if(isset($_POST['submit']))
+{
+
+	$name = mysqli_real_escape_string($conn,$_POST['name']);
+	$email = mysqli_real_escape_string($conn,$_POST['email']);
+$password = mysqli_real_escape_string($conn,$_POST['password']);
+	$cpassword = mysqli_real_escape_string($conn,$_POST['cpassword']);
+
+	if(empty($name)||empty($email)||empty($password)||empty($cpassword))
+	{
+		echo "empty fields";
+	}
+
+
+	else
+	{
+		$sql = "SELECT * FROM user WHERE name = '$name'";
+		$result = mysqli_query($conn,$sql);
+		$resultCheck = mysqli_num_rows($result);
+
+
+		if($resultCheck > 0)
+		{
+			echo "Id already in use";
+
+
+		}
+
+		else
+		{
+
+
+					$sql = "INSERT into user (name,email,password,cpassword) VALUES ('$name','$email','$password','$cpassword');";
+
+					mysqli_query($conn,$sql);
+
+
+					header("Location:./login.html");
+
+
+
+
+
+
+
+		}
+
+	}
+
+}
+
+else
+{
+	//header("Location:./index.php?login=error");
+	exit();
+}
+
+?>
