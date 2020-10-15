0fff1372b335dc853454a9668a1cf76b6e37ed5c
shukra <shukrafathima024@gmail.com>
2020-10-15 10:27:49 +0530

    TDA-09

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "todo";
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+?>
